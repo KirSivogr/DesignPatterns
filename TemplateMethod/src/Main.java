e9ce87888920ab665326b9aee7804bbeca51740e
Kirill Sivograkov <110250397+KirSivogr@users.noreply.github.com>
2022-12-25 22:51:48 +0300

    add templateMethod

---
+++ TemplateMethod/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Basketball ();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
